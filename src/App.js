3464e0d815e448794e0edeec22c9816fd9676924
bodaghialib4 <bodaghialib4@gmail.com>
2020-11-15 16:46:51 +0330

    add Home.js component into App.j

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react';
+import {Switch, Route} from "react-router-dom";
+
 import Header from "./components/Header";
+import Home from "./components/Home";
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header/>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+        </Switch>
       </div>
     );
   }
